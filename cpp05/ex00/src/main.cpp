6144ad3f2a8dd4e79044d8dc6695f052ed5dffc0
makelannika <makelannika@gmail.com>
2024-11-06 21:16:21 +0200

    printing fixed

--- cpp05/ex00/src/main.cpp
+++ cpp05/ex00/src/main.cpp
@@ -6,34 +6,35 @@
 /*   By: amakela <amakela@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 15:50:49 by amakela           #+#    #+#             */
-/*   Updated: 2024/11/06 17:52:10 by amakela          ###   ########.fr       */
+/*   Updated: 2024/11/06 21:15:47 by amakela          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
 int	main() {
-	Bureaucrat b1("Bob", 2), b2("Rob", 149), b3;
-	std::cout << b1 << std::endl << b2 << std::endl << b3 << std::endl;
+	Bureaucrat bob("Bob", 2), rob("Rob", 149);
+	std::cout << bob << rob << std::endl;
+	
 	try {
-		Bureaucrat b3("Todd", 151);
+		Bureaucrat todd("Todd", 151);
 	} catch (std::exception& e) {
 		std::cerr << e.what() << std::endl;
 	}
 	
 	try {
-		b1.incrementGrade();
-		b2.decrementGrade();
-		b1.incrementGrade();
+		bob.incrementGrade();
+		rob.decrementGrade();
+		bob.incrementGrade();
 	} catch (std::exception& e) {
 			std::cerr << e.what() << std::endl;
 	}
-	std::cout << b1 << std::endl << b2 << std::endl;
+	std::cout << bob << rob << std::endl;
 
 	try {
-		b2.decrementGrade();
+		rob.decrementGrade();
 	} catch (std::exception& e) {
 			std::cerr << e.what() << std::endl;
 	}
-	std::cout << b1 << std::endl << b2 << std::endl;
+	std::cout << bob << rob << std::endl;
 }
