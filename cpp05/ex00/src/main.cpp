94aef86d3bfb4d18e4a88cb048f771f56978495b
Annika Mäkelä <makelannika@gmail.com>
2024-11-07 12:24:57 +0200

    tests added

--- cpp05/ex00/src/main.cpp
+++ cpp05/ex00/src/main.cpp
@@ -6,35 +6,51 @@
 /*   By: amakela <amakela@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 15:50:49 by amakela           #+#    #+#             */
-/*   Updated: 2024/11/06 21:15:47 by amakela          ###   ########.fr       */
+/*   Updated: 2024/11/07 12:19:38 by amakela          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
+void	test2() {
+	Bureaucrat	todd("Todd", 151);
+}
+
+void	test1() {
+	Bureaucrat	todd("Todd", 0);
+}
+
 int	main() {
 	Bureaucrat bob("Bob", 2), rob("Rob", 149);
-	std::cout << bob << rob << std::endl;
 	
-	try {
-		Bureaucrat todd("Todd", 151);
-	} catch (std::exception& e) {
-		std::cerr << e.what() << std::endl;
+	/* creating bureaucrats with invalid grades */
+	for (auto test : {test1, test2}) {
+		try {
+			test();
+		} catch (std::exception& e) {
+			std::cerr << e.what();
+		}
 	}
+	std::cout << std::endl;
 	
-	try {
-		bob.incrementGrade();
-		rob.decrementGrade();
-		bob.incrementGrade();
-	} catch (std::exception& e) {
-			std::cerr << e.what() << std::endl;
-	}
+	/* bureaucrat info before */
 	std::cout << bob << rob << std::endl;
 
+	/* incrementing Bob's grade twice */
 	try {
+		bob.incrementGrade();
+		bob.incrementGrade();
+	} catch (std::exception& e) {
+			std::cerr << e.what() << std::endl;
+	}
+
+	/* decrementing Rob's grade twice */
+	try {
+		rob.decrementGrade();
 		rob.decrementGrade();
 	} catch (std::exception& e) {
 			std::cerr << e.what() << std::endl;
 	}
+	/* bureaucrat info after */
 	std::cout << bob << rob << std::endl;
 }
