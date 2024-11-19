672c9f9fd77ac1ae102a84a0fcb42f74bf4478bf
makelannika <makelannika@gmail.com>
2024-11-19 14:36:47 +0200

    min & max value tests added

--- cpp06/ex00/src/main.cpp
+++ cpp06/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amakela <amakela@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 13:45:53 by amakela           #+#    #+#             */
-/*   Updated: 2024/11/14 16:38:28 by amakela          ###   ########.fr       */
+/*   Updated: 2024/11/19 14:34:52 by amakela          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,30 @@ int main(int argc, char** argv)
         std::string input(argv[1]);
         ScalarConverter::convert(input);
     }
+	else
+	{
+		std::cout << "INT MAX" << std::endl;
+		ScalarConverter::convert(std::to_string(std::numeric_limits<int>::max()));
+		std::cout << std::endl;
+		std::cout << "INT MIN" << std::endl;
+		ScalarConverter::convert(std::to_string(std::numeric_limits<int>::min()));
+		std::cout << std::endl;
+		std::cout << "FLOAT MAX" << std::endl;
+		ScalarConverter::convert(std::to_string(std::numeric_limits<float>::max()) + 'f');
+		std::cout << std::endl;
+		std::cout << "FLOAT MIN" << std::endl;
+		ScalarConverter::convert(std::to_string(std::numeric_limits<float>::min()) + 'f');
+		std::cout << std::endl;
+		std::cout << "FLOAT LOWEST" << std::endl;
+		ScalarConverter::convert(std::to_string(std::numeric_limits<float>::lowest()) + 'f');
+		std::cout << std::endl;
+		std::cout << "DOUBLE MAX" << std::endl;
+		ScalarConverter::convert(std::to_string(std::numeric_limits<double>::max()));
+		std::cout << std::endl;
+		std::cout << "DOUBLE MIN" << std::endl;
+		ScalarConverter::convert(std::to_string(std::numeric_limits<double>::min()));
+		std::cout << std::endl;
+		std::cout << "DOUBLE LOWEST" << std::endl;
+		ScalarConverter::convert(std::to_string(std::numeric_limits<double>::lowest()));
+	}
 }
