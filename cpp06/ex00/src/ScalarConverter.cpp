4a97c0b48ede416c05b7a83fcee37219b447b17d
makelannika <makelannika@gmail.com>
2024-11-23 11:31:43 +0200

    unnecessary checks removed

--- cpp06/ex00/src/ScalarConverter.cpp
+++ cpp06/ex00/src/ScalarConverter.cpp
@@ -6,46 +6,37 @@
 /*   By: amakela <amakela@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 14:58:40 by amakela           #+#    #+#             */
-/*   Updated: 2024/11/21 20:12:15 by amakela          ###   ########.fr       */
+/*   Updated: 2024/11/23 11:26:00 by amakela          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
 template<typename T>
-void displayPseudoLiterals(T value) {
-	std::cout << "char: impossible" << std::endl << "int: impossible" << std::endl;
-	if (std::isnan(value))
-		std::cout << "float: nanf" << std::endl << "double: nan" << std::endl;
-	else {
-		std::cout << "float: " << (value < 0 ? "" : "+") << static_cast<float>(value)  << "f" << std::endl;
-		std::cout << "double: " << (value < 0 ? "" : "+") <<  static_cast<double>(value) << std::endl;
-	}
-}
-
-template<typename T>
 void display(T value) {
-	if (std::isnan(value) || std::isinf(value)) {
-		displayPseudoLiterals(value);
-		return ;
-	}
-	if (value < 0 || value > 127)
+	if (value < 0 || value > 127 || value != value)
         std::cout << "char: impossible" << std::endl;
 	else if (value < 32 || value > 126)
         std::cout << "char: non displayable" << std::endl;
 	else {
         std::cout << "char: '" << static_cast<char>(value) << "'" << std::endl;
 	}
-	if (value < static_cast<double>(INT_MIN) || value > static_cast<double>(INT_MAX))
+	if (value < static_cast<double>(INT_MIN) || value > static_cast<double>(INT_MAX) || value != value)
         std::cout << "int: impossible" << std::endl;
 	else {
         std::cout << "int: " << static_cast<int>(value) << std::endl;
 	}
-	if (value < static_cast<double>(-FLT_MAX) || value > static_cast<double>(FLT_MAX))
+	if (value < static_cast<double>(-DBL_MAX) || value > static_cast<double>(DBL_MAX))
+		std::cout << "float: " << (value < 0 ? "" : "+") << static_cast<float>(value)  << "f" << std::endl;
+	else if (value < static_cast<double>(-FLT_MAX) || value > static_cast<double>(FLT_MAX))
 		std::cout << "float: impossible" << std::endl;
-	else
+	else {
 		std::cout << "float: " << static_cast<float>(value) << "f" << std::endl;
-	std::cout << "double: " << static_cast<double>(value) << std::endl;
+	}
+	if (value < static_cast<double>(-DBL_MAX) || value > static_cast<double>(DBL_MAX))
+		std::cout << "double: " << (value < 0 ? "" : "+") << static_cast<double>(value) << std::endl;
+	else
+		std::cout << "double: " << static_cast<double>(value) << std::endl;
 }
 
 bool lengthIsEqual(std::string input, size_t lenParsed) {
@@ -63,11 +54,6 @@ int getType(std::string input) {
     
     if (lengthIsEqual(input, 1) && (input[0] < '0' || input[0] > '9'))
         return CHAR;
-    if (input == "+inff" || input == "-inff" || input == "nanf")
-        return FLOAT;
-    if (input == "+inf" || input == "-inf" || input == "nan")
-        return DOUBLE;
-        
     try {
         std::stoi(input, &idx);
         if (lengthIsEqual(input, idx))
