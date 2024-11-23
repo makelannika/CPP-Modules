1640bcbe6f64f628d813e94aed48977d57980886
makelannika <makelannika@gmail.com>
2024-11-23 12:15:41 +0200

    main

--- cpp06/ex01/src/main.cpp
+++ cpp06/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amakela <amakela@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 22:28:39 by amakela           #+#    #+#             */
-/*   Updated: 2024/11/17 14:14:07 by amakela          ###   ########.fr       */
+/*   Updated: 2024/11/23 12:14:51 by amakela          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,10 @@
 
 int main() {
     Data*       ptr = new Data{"Data", 2};
-    Data*       deserialized;
-    uintptr_t   serialized;
 
     std::cout << "original pointer:\t" << ptr << std::endl;
-    serialized = Serializer::serialize(ptr);
-    deserialized = Serializer::deserialize(serialized);
+    uintptr_t serialized = Serializer::serialize(ptr);
+    Data* deserialized = Serializer::deserialize(serialized);
     std::cout << "deserialized pointer:\t" << deserialized << std::endl;
     std::cout << "name: " << deserialized->name << std::endl;
     std::cout << "value: " << deserialized->value << std::endl;
