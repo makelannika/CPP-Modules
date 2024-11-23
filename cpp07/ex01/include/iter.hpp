3c7159eae9e9d2c6fd1b790735c9255d2263d342
makelannika <makelannika@gmail.com>
2024-11-23 17:06:56 +0200

    check added for nullptr

--- cpp07/ex01/include/iter.hpp
+++ cpp07/ex01/include/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: amakela <amakela@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 11:38:42 by amakela           #+#    #+#             */
-/*   Updated: 2024/11/22 13:16:51 by amakela          ###   ########.fr       */
+/*   Updated: 2024/11/23 16:52:06 by amakela          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,9 @@ void print(T element) { std::cout << element << std::endl; }
 
 template<typename T, typename F>
 void iter(T* array, size_t len, F func) {
-	for (size_t i = 0; i < len; i++) {
-		func(array[i]);
+	if (array != nullptr) {
+		for (size_t i = 0; i < len; i++)
+			func(array[i]);
 	}
 }
 
