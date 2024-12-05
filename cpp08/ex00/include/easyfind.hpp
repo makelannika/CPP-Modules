7f0cc1fe0b9d229e454f68b2a3ea93fc225ec728
Annika Mäkelä <makelannika@gmail.com>
2024-12-05 23:13:15 +0200

    unnecessary code removed

--- cpp08/ex00/include/easyfind.hpp
+++ cpp08/ex00/include/easyfind.hpp
@@ -7,16 +7,12 @@
 
 template<typename T>
 void easyfind(const T& container, int value) {
-	using type = typename T::value_type;
-
-	if constexpr (std::is_same_v<type, int>) {
-		auto iterator = std::ranges::find(container, value);
-		if (iterator == container.end())
-			throw std::out_of_range("value " + std::to_string(value) + " not found");
-		std::cout << "value " << value << " found\n";
-	}
-	else
+	if constexpr (!std::is_same_v<typename T::value_type, int>) 
 		throw std::invalid_argument("container needs to hold type int");
+		
+	if (std::ranges::find(container, value) == container.end())
+		throw std::out_of_range("value " + std::to_string(value) + " not found");
+	std::cout << "value " << value << " found\n";
 }
 
 #endif
