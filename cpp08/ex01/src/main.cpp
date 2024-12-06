48d831fd4e9440d1a28b9f693911a696e52ff414
Annika Mäkelä <makelannika@gmail.com>
2024-12-06 23:36:02 +0200

    main

--- cpp08/ex01/src/main.cpp
+++ cpp08/ex01/src/main.cpp
@@ -7,15 +7,15 @@ int main() {
 	try {
 		for (int i = 0; i < 183; i++)
 			numbers.addNumber(i);
+		std::cout << numbers.longestSpan() << "\n";
 	} catch (std::exception& e) {
 		std::cerr << e.what() << "\n";
 	}
-	std::cout << numbers.longestSpan() << "\n";
 
-	// Span span2(span);
-	// try {
-	// 	span2.addNumber(4);	
-	// } catch (std::exception& e) {
-	// 	std::cerr << e.what() << "\n";
-	// }
+	Span span2(numbers);
+	try {
+		span2.addNumber(4);	
+	} catch (std::exception& e) {
+		std::cerr << e.what() << "\n";
+	}
 }
