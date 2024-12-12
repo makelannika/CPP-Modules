043fe88b1571f561c1a8ecb45a6d0590c243d51e
makelannika <makelannika@gmail.com>
2024-12-12 20:31:25 +0200

    returning iterator

--- cpp08/ex00/include/easyfind.hpp
+++ cpp08/ex00/include/easyfind.hpp
@@ -1,22 +1,14 @@
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
-#include <stdexcept>
 #include <iostream>
 #include <ranges>
 
 template<typename T>
-void easyfind(const T& container, int value) {
-	using type = typename T::value_type;
+typename T::const_iterator easyfind(const T& container, int value) {
+	static_assert(std::is_same_v<typename T::value_type, int>, "Container must hold type int");
 
-	if constexpr (std::is_same_v<type, int>) {
-		auto iterator = std::ranges::find(container, value);
-		if (iterator == container.end())
-			throw std::out_of_range("value " + std::to_string(value) + " not found");
-		std::cout << "value " << value << " found\n";
-	}
-	else
-		throw std::invalid_argument("container needs to hold type int");
+	return std::ranges::find(container, value);
 }
 
 #endif
--- cpp08/ex00/src/main.cpp
+++ cpp08/ex00/src/main.cpp
@@ -1,30 +1,26 @@
 #include "easyfind.hpp"
 #include <vector>
+#include <array>
 #include <deque>
 #include <list>
 
 int main() {
-	{
-		std::deque<int> dq = {8, 11, 6, 3};
-		std::list<int> lst = {-2, 0, 1, -12};
-		std::vector<int> vctr = {2, 10, 4, 5, 7};
+	std::deque<int> dq = {8, 11, 6, 3};
+	std::list<int> lst = {-2, 0, 1, -12};
+	std::array<int, 3> arr = {15, 100, 9};
+	std::vector<int> vctr;
 
-		try {
-			easyfind(dq, 3);
-			easyfind(lst, 0);
-			easyfind(vctr, 2);
-			easyfind(vctr, 18);
-		} catch (std::exception& e) {
-			std::cerr << e.what() << "\n";
-		}
-	}
-
-	std::cout << "\n";
-	std::vector<float> vctr = {2.1, 4.5, 6.7};
-	
 	try {
-		easyfind(vctr, 2);
+		auto dqit = easyfind(dq, 11);
+			std::cout << "value " << (dqit != dq.end() ? std::to_string(*dqit) + " found\n" : "not found\n");
+		auto lstit = easyfind(lst, -2);
+			std::cout << "value " << (lstit != lst.end() ? std::to_string(*lstit) + " found\n" : "not found\n");
+		auto arrit = easyfind(arr, 102);
+			std::cout << "value "  << (arrit != arr.end() ? std::to_string(*arrit) + " found\n" : "not found\n");
+		auto vctrit = easyfind(vctr, 500);
+			std::cout << "value " << (vctrit != vctr.end() ? std::to_string(*vctrit) + " found\n" : "not found\n");
 	} catch (std::exception& e) {
-		std::cerr << e.what() << "\n";
+		std::cout << e.what() << "\n";
 	}
 }
+
