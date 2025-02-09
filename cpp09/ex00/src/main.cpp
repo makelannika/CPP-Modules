6e0514745332e77a9d151bdb45a9c476ff151d37
Annika <makelannika@gmail.com>
2025-02-09 15:27:21 +0200

    cpp09

---
+++ cpp09/ex00/src/main.cpp
@@ -0,0 +1,14 @@
+#include "BitcoinExchange.hpp"
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    if (argc != 2) {
+        std::cout << "Program takes a file as argument\n";
+    }
+}
+
+// ex00: map [date, rate]
+// ex01: stack
+// ex02: 
