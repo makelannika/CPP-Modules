e49b64e3fd1d0f12b5cc6e194ea8a01f500d7237
makelannika <makelannika@gmail.com>
2025-02-12 12:40:16 +0200

    ex00

--- cpp08/ex00/include/easyfind.hpp
+++ cpp08/ex00/include/easyfind.hpp
@@ -5,7 +5,7 @@
 #include <ranges>
 
 template<typename T>
-typename T::const_iterator easyfind(const T& container, int value) {
+typename T::iterator easyfind(T& container, int value) {
 	static_assert(std::is_same_v<typename T::value_type, int>, "Container must hold type int");
 
 	return std::ranges::find(container, value);
