e859090a3059a94cd467b7c742540701aedb6897
makelannika <makelannika@gmail.com>
2025-02-13 16:33:03 +0200

    ex02

--- cpp08/ex02/include/MutantStack.hpp
+++ cpp08/ex02/include/MutantStack.hpp
@@ -6,14 +6,12 @@
 template<class T,class Container = std::deque<T> >
 class MutantStack : public std::stack<T, Container> {
     public:
-        MutantStack() {}
-        ~MutantStack() {}
-        MutantStack(const MutantStack& other) : std::stack<T, Container>(other) {}
-        MutantStack&    operator=(const MutantStack& other) {
-            if (this != &other)
-                std::stack<T, Container>::operator=(other);
-            return *this;
-        }
+        using std::stack<T, Container>::stack;
+
+		MutantStack() = default;
+		~MutantStack() = default;
+        MutantStack(const MutantStack& other) = default;
+        MutantStack&    operator=(const MutantStack& other) = default;
 
         using iterator = typename Container::iterator;
 		using const_iterator = typename Container::const_iterator;
