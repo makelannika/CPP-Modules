6a6605732efd1bf1ad2b161b4dcdca4b038d06b8
makelannika <makelannika@gmail.com>
2025-03-05 16:04:29 +0200

    ex02

--- cpp09/ex02/src/PmergeMe.cpp
+++ cpp09/ex02/src/PmergeMe.cpp
@@ -70,11 +70,61 @@ void    PmergeMe::sort()
     sort();
     m_unitSize /= 2;
 
-	// std::vector<int>	main;
-	// std::vector<int>	pend;
+	std::cout << "UNIT SIZE: " << m_unitSize << "\n";
+	if (m_vector.size() / m_unitSize < 3)
+		return;
 
+	int 				units = m_vector.size() / m_unitSize; // 5
+	bool				odd = units % 2; // 1
+	std::vector<int>	main;
+	std::vector<int>	pend;
+	std::vector<int>	odd_element;
 
+	// for (int i = 0; i < m_unitSize * 2; i++)
+	// 	main.push_back(m_vector[i]);
 
+	main.push_back(m_vector[m_unitSize - 1]);
+	main.push_back(m_vector[2 * m_unitSize - 1]);
+
+	std::cout << "(b1 a1) MAIN: ";
+	for (int i: main)
+		std::cout << i << " ";
+	
+	std::cout << "\n\n";
+
+	// for (int i = 2; i < units - odd; i++) {
+	// 	for (int j = 0; j < m_unitSize; j++) {
+	// 		if (i % 2 == 0)
+	// 			pend.push_back(m_vector[i * m_unitSize + j]);
+	// 		else
+	// 			main.push_back(m_vector[i * m_unitSize + j]);
+	// 	}	
+	// }
+
+	for (int i = 2; i < units - odd; i++) {
+		if (i % 2 == 0)
+			pend.push_back(m_vector[i * m_unitSize + m_unitSize - 1]);
+		else
+			main.push_back(m_vector[i * m_unitSize + m_unitSize - 1]);
+	}
+	
+	for (int i = 0; i < pend.size(); i++) {
+		auto pos = std::lower_bound(main.begin(), main.end(), pend[i]);
+		for (int j = 0; j < m_unitSize; j++) {
+			
+		}
+	}
+	std::cout << "MAIN: ";
+	for (int i: main)
+		std::cout << i << " ";
+	
+	std::cout << "\n\n";
+
+	std::cout << "PEND: ";
+	for (int i: pend)
+		std::cout << i << " ";
+
+	std::cout << "\n\n";
 }
 
 
